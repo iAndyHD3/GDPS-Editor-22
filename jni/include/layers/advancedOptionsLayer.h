3e530f11c1423fe865c53cddfd10b4fa0b6800cd
xhayper <hayper1919@gmail.com>
2022-06-03 13:09:27 +0700

    feat: Make it work with VSCode 100%

--- jni/include/layers/advancedOptionsLayer.h
+++ jni/include/layers/advancedOptionsLayer.h
@@ -2,18 +2,15 @@
 #include "cocos2d.h"
 #include "gd.h"
 
-class advancedOptionsLayer : public CCLayerColor {
-	public:
+class advancedOptionsLayer : public CCLayerColor
+{
+public:
+	void show();
 
+	bool init();
 
+	static advancedOptionsLayer *create();
 
-
-		void show();
-
-		bool init();
-
-		static advancedOptionsLayer* create();
-
-		virtual void keyBackClicked();
-		void close();
+	virtual void keyBackClicked();
+	void close();
 };
